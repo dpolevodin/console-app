7f0e40e7ad97024e504bb47cd783dfd3191287ed
Полеводин Дмитрий Игоревич (4094029) <dipolevodin@vtb.ru>
2024-04-24 15:20:59 +0600

    colored result added

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+import chalk from "chalk";
+
 if (process.argv.length < 3) {
-  console.error("Укажите величину в дюймах");
+  console.error(chalk.red("Укажите величину в дюймах"));
   process.exit(1);
 }
 const inches = parseFloat(process.argv[2]);
 if (!inches) {
-  console.error("Укажите дюймы в виде числа");
+  console.error(chalk.red("Укажите дюймы в виде числа"));
   process.exit(1);
 }
 const centimeters = inches * 2.54;
-console.log(`${inches} >>> ${centimeters} см`);
+console.log(`${chalk.blue(inches)} >>> ${chalk.green(centimeters)} см`);
